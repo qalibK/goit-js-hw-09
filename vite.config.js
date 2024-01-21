a6cb023936e29c1412a051dabae73fa8723656cf
qalibK <bvomusak01@gmail.com>
2024-01-21 21:57:13 +0100

    feat: some edits

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
+
 export default defineConfig(({ command }) => {
   return {
     define: {
